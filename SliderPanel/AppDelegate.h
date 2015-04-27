b2c9eb443b6687c9624d9df5f03f0b26f8883f04
VoldLiu <liu_heyu@126.com>
2015-04-27 16:20:20 +0800

    Initial Commit

---
+++ SliderPanel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SliderPanel
+//
+//  Created by allen on 27/4/15.
+//  Copyright (c) 2015 allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SliderPanel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SliderPanel
+//
+//  Created by allen on 27/4/15.
+//  Copyright (c) 2015 allen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
